82281a1ca6ae3feabe0a4d010c648810ce8d918d
Jihye Seo <rabierre@gmail.com>
2017-01-11 10:13:03 +0900

    Merge code

--- token.go
+++ token.go
@@ -146,3 +146,39 @@ func (t Token) Priority() int {
 	}
 	return LowestPriority
 }
+
+var Keywords = [...]string{
+	IF:     "if",
+	ELSE:   "else",
+	FOR:    "for",
+	FUNC:   "func",
+	INT:    "int",
+	DOUBLE: "double",
+	RETURN: "return",
+	TRUE:   "true",
+	FALSE:  "false",
+
+	LPAREN: "(",
+	RPAREN: ")",
+	LBRACE: "{",
+	RBRACE: "}",
+
+	COMMENT_SLASH: "//",
+	SEMI_COLON:    ";",
+	COMMA:         ",",
+
+	PLUS:   "+",
+	MINUS:  "-",
+	MULTI:  "*",
+	DIVIDE: "/",
+	ASSIGN: "=",
+	LESS:   "<",
+	GRT:    ">",
+	LEQ:    "<=",
+	GEQ:    ">=",
+	EQ:     "==",
+	NEQ:    "!=",
+
+	SPACE: " ",
+	VOID:  "void",
+}
